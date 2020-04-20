9d9157d7bf2312dce008c76b20e43f9f81c2ddb9
Harry Merritt <harry@merritt.es>
2020-04-20 02:20:42 +0100

    Add GET all to actionRouter

--- resources/actionRouter.js
+++ resources/actionRouter.js
@@ -3,4 +3,35 @@ const db = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
+//  Get all actions
+//  -> array
+router.get("/", validateActionId(), (req, res, next) => {
+    res.send(req.action);
+});
+
+//  Check if action id exists
+function validateActionId() {
+    return (req, res, next) => {
+        db.get((req.params.id || null))
+            .then((action) => {
+                //  Check if action exists
+                if (action) {
+                    //  All good -> continue
+                    //  Add action to request object
+                    req.action = action;
+                    next();
+                } else {
+                    //  Project does not exist
+                    res.status(400).json({
+                        message: "invalid action id",
+                    });
+                }
+            })
+            .catch((error) => {
+                //  Server failed when accessing database
+                next(error);
+            });
+    };
+}
+
 module.exports = router;
